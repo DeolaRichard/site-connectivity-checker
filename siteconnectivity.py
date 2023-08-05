1426bcdf757ca774b79a633f1ac7fe37c38c59d4
DeolaRichard <adekunlerichie5@gmail.com>
2023-08-05 01:43:26 +0100

    wrote block of codes to check for the connection status of a site

--- siteconnectivity.py
+++ siteconnectivity.py
@@ -1,12 +1,12 @@
-import urllib.request as urllib
+import urllib.request as urllib  # import module to check for url connectivity
 
 
-def main(url):
+def main(url):  # function to check for connectivity
     print("Checking connectivity ")
 
     response = urllib.urlopen(url)
     print("Connected to", url, "successfully")
-    print("The response code was: ", response.getcode())
+    print("The response code was: ", response.getcode())  # code to get response code
 
 
 print("This is a URL connectivity checker")
